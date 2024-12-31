2158dd7458f635428fe767cbac7e3a9615f7e009
Leonardo Jung <155767888+jungleo2001@users.noreply.github.com>
2024-12-31 02:38:42 -0300

    Create tunner.py

---
+++ tunner.py
@@ -0,0 +1,12 @@
+#importing some necessary shit
+
+import numpy as np 
+import sounddevice as sd 
+from scipy.fftpack import fft
+
+def main():
+    print("Hello world, the imports have worked")
+
+
+if __name__ == "__main__":
+    main() 
